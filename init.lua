0c8740d007d0a62f7ae99486e731737a7764ef6e
Gratla <philippleeb@gmail.com>
2025-05-12 22:10:35 +0200

    Add VimBeGood

--- init.lua
+++ init.lua
@@ -261,6 +261,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'ThePrimeagen/vim-be-good',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
