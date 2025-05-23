007f1e26d991d2bfe22291de23baf14742c49783
Gratla <philippleeb@gmail.com>
2025-05-23 20:36:04 +0200

    Added nvim hardtime and removed some leftovers from initialisation of config

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,20 +1,37 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+-- Set <space> as the leader key
 
-local harpoon = require("harpoon")
+local harpoon = require 'harpoon'
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "[h]arpoon [a]dd" } )
-vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set('n', '<leader>ha', function()
+  harpoon:list():add()
+end, { desc = '[h]arpoon [a]dd' })
+vim.keymap.set('n', '<leader>he', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
 
-vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+vim.keymap.set('n', '<leader>h1', function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set('n', '<leader>h2', function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set('n', '<leader>h3', function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set('n', '<leader>h4', function()
+  harpoon:list():select(4)
+end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<leader>hp>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<leader>hn>", function() harpoon:list():next() end)
+vim.keymap.set('n', '<leader>hp', function()
+  harpoon:list():prev()
+end)
+vim.keymap.set('n', '<leader>hn', function()
+  harpoon:list():next()
+end)
 
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[g]it [s]tatus" })
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[g]it [s]tatus' })
