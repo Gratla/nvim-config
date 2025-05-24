bd1621402d3031918b15e32a3cc8565ec0108b89
Gratla <philippleeb@gmail.com>
2025-05-25 00:30:33 +0200

    Added typescript-tools plugin

--- init.lua
+++ init.lua
@@ -620,7 +620,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         jdtls = {},
-        ts_ls = {},
+        -- ts_ls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
